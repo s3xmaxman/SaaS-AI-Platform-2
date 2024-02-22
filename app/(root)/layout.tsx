8800417c9b0ad10e213fcdf791c6d312eb9d4c0e
s3xmaxman <badside3919@gmail.com>
2024-02-22 16:59:06 +0900

    add Sidebar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
+
 
 const layout = ({ children }: { children: React.ReactNode}) => {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className='root-container'>
         <div className='wrapper'>
           {children}
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+    SheetTitle,
+    SheetDescription,
+    SheetHeader,
+} from '@/components/ui/sheet'
+
+
+const MobileNav = () => {
+  return (
+    <div>MobileNav</div>
+  )
+}
+
+export default MobileNav
