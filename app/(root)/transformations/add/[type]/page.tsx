ecf16bae11a447ccf3de2963918fb78152c80a49
s3xmaxman <badside3919@gmail.com>
2024-02-27 07:55:15 +0900

    implement clerk webhooks

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/components/shared/Header'
+import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+
+const AddTransformationTypePage = ({ params: { type }}: SearchParamProps) => {
+  const transformation = transformationTypes[type]
+  return (
+    <>
+    <Header 
+      title={transformation.title}
+      subtitle={transformation.subTitle}
+    />
+    <section className='mt-10'>
+      <TransformationForm />
+    </section>
+  </>
+  )
+}
+
+export default AddTransformationTypePage
