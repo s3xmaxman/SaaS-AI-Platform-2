57060dadaf114366c9160315cdd34f7adadff9f6
s3xmaxman <badside3919@gmail.com>
2024-02-27 06:49:18 +0900

    implement clerk webhooks

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,42 +1,32 @@
-
-import mongoose, { Mongoose} from "mongoose";
-
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-// Mongooseの接続情報を持つインターフェース
 interface MongooseConnection {
-    conn: Mongoose | null; // 接続オブジェクト
-    promise: Promise<Mongoose> | null; // 接続プロミス
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-// グローバル変数からキャッシュされた接続情報を取得
-let cached: MongooseConnection = ( global as any ).mongoose 
+let cached: MongooseConnection = (global as any).mongoose
 
-// キャッシュが存在しない場合は新たに作成
-if(!cached){
-    cached = ( global as any ).mongoose = { conn: null, promise: null }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-// MongoDBへの接続関数
-export const connectionToMongoDB = async () => {
-    // 既に接続が存在する場合はそれを返す
-    if(cached.conn) return cached.conn
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    // MongoDBのURLが存在しない場合はエラーを投げる
-    if(!MONGODB_URL) throw new Error("Missing MONGODB_URL")
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    // 接続プロミスが存在しない場合は新たに作成
-    cached.promise = 
+  cached.promise = 
     cached.promise || 
-    mongoose.connect(MONGODB_URL,{
-        dbName: "imaginify", // データベース名
-        bufferCommands: false, // コマンドバッファリングを無効化
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
     })
 
-    // 接続プロミスを待機
-    cached.conn = await cached.promise
+  cached.conn = await cached.promise;
 
-    // 接続オブジェクトを返す
-    return cached.conn;
+  return cached.conn;
 }
