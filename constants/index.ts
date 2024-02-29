45d706f14a7c20dbcacee3510f3ce9dbcc60725e
s3xmaxman <badside3919@gmail.com>
2024-02-29 11:32:25 +0900

    add image server actions

--- constants/index.ts
+++ constants/index.ts
@@ -1,193 +1,193 @@
 export const navLinks = [
-    {
-      label: "ホーム",
-      route: "/",
-      icon: "/assets/icons/home.svg",
-    },
-    {
-      label: "イメージ修復",
-      route: "/transformations/add/restore",
-      icon: "/assets/icons/image.svg",
-    },
-    {
-      label: "ジェネレーティブ塗りつぶし",
-      route: "/transformations/add/fill",
-      icon: "/assets/icons/stars.svg",
-    },
-    {
-      label: "オブジェクト除去",
-      route: "/transformations/add/remove",
-      icon: "/assets/icons/scan.svg",
-    },
-    {
-      label: "オブジェクトのカラー変換",
-      route: "/transformations/add/recolor",
-      icon: "/assets/icons/filter.svg",
-    },
-    {
-      label: "背景除去",
-      route: "/transformations/add/removeBackground",
-      icon: "/assets/icons/camera.svg",
-    },
-    {
-      label: "プロフィール",
-      route: "/profile",
-      icon: "/assets/icons/profile.svg",
-    },
-    {
-      label: "クレジットを購入",
-      route: "/credits",
-      icon: "/assets/icons/bag.svg",
-    },
-  ];
-  
-  export const plans = [
-    {
-      _id: 1,
-      name: "フリープラン",
-      icon: "/assets/icons/free-plan.svg",
-      price: 0,
-      credits: 20,
-      inclusions: [
-        {
-          label: "無料の２０クレジット",
-          isIncluded: true,
-        },
-        {
-          label: "サービスへの基本的なアクセス",
-          isIncluded: true,
-        },
-        {
-          label: "優先的なサポート",
-          isIncluded: false,
-        },
-        {
-          label: "優先的なアップデート",
-          isIncluded: false,
-        },
-      ],
-    },
-    {
-      _id: 2,
-      name: "プロプラン",
-      icon: "/assets/icons/free-plan.svg",
-      price: 40,
-      credits: 120,
-      inclusions: [
-        {
-          label: "120 クレジット",
-          isIncluded: true,
-        },
-        {
-          label: "サービスへの完全アクセス",
-          isIncluded: true,
-        },
-        {
-          label: "優先的カスタマーサポート",
-          isIncluded: true,
-        },
-        {
-          label: "優先的なアップデート",
-          isIncluded: false,
-        },
-      ],
-    },
-    {
-      _id: 3,
-      name: "プレミアムプラン",
-      icon: "/assets/icons/free-plan.svg",
-      price: 199,
-      credits: 2000,
-      inclusions: [
-        {
-          label: "2,000 クレジット",
-          isIncluded: true,
-        },
-        {
-          label: "完全なサービスへのアクセス",
-          isIncluded: true,
-        },
-        {
-          label: "優先的なカスタマーサポート",
-          isIncluded: true,
-        },
-        {
-          label: "優先的なアップデート",
-          isIncluded: true,
-        },
-      ],
-    },
-  ];
-  
-  export const transformationTypes = {
-    restore: {
-      type: "restore",
-      title: "Restore Image",
-      subTitle: "Refine images by removing noise and imperfections",
-      config: { restore: true },
-      icon: "image.svg",
-    },
-    removeBackground: {
-      type: "removeBackground",
-      title: "Background Remove",
-      subTitle: "Removes the background of the image using AI",
-      config: { removeBackground: true },
-      icon: "camera.svg",
-    },
-    fill: {
-      type: "fill",
-      title: "Generative Fill",
-      subTitle: "Enhance an image's dimensions using AI outpainting",
-      config: { fillBackground: true },
-      icon: "stars.svg",
-    },
-    remove: {
-      type: "remove",
-      title: "Object Remove",
-      subTitle: "Identify and eliminate objects from images",
-      config: {
-        remove: { prompt: "", removeShadow: true, multiple: true },
+  {
+    label: "Home",
+    route: "/",
+    icon: "/assets/icons/home.svg",
+  },
+  {
+    label: "Image Restore",
+    route: "/transformations/add/restore",
+    icon: "/assets/icons/image.svg",
+  },
+  {
+    label: "Generative Fill",
+    route: "/transformations/add/fill",
+    icon: "/assets/icons/stars.svg",
+  },
+  {
+    label: "Object Remove",
+    route: "/transformations/add/remove",
+    icon: "/assets/icons/scan.svg",
+  },
+  {
+    label: "Object Recolor",
+    route: "/transformations/add/recolor",
+    icon: "/assets/icons/filter.svg",
+  },
+  {
+    label: "Background Remove",
+    route: "/transformations/add/removeBackground",
+    icon: "/assets/icons/camera.svg",
+  },
+  {
+    label: "Profile",
+    route: "/profile",
+    icon: "/assets/icons/profile.svg",
+  },
+  {
+    label: "Buy Credits",
+    route: "/credits",
+    icon: "/assets/icons/bag.svg",
+  },
+];
+
+export const plans = [
+  {
+    _id: 1,
+    name: "Free",
+    icon: "/assets/icons/free-plan.svg",
+    price: 0,
+    credits: 20,
+    inclusions: [
+      {
+        label: "20 Free Credits",
+        isIncluded: true,
       },
-      icon: "scan.svg",
-    },
-    recolor: {
-      type: "recolor",
-      title: "Object Recolor",
-      subTitle: "Identify and recolor objects from the image",
-      config: {
-        recolor: { prompt: "", to: "", multiple: true },
+      {
+        label: "Basic Access to Services",
+        isIncluded: true,
       },
-      icon: "filter.svg",
+      {
+        label: "Priority Customer Support",
+        isIncluded: false,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "Pro Package",
+    icon: "/assets/icons/free-plan.svg",
+    price: 40,
+    credits: 120,
+    inclusions: [
+      {
+        label: "120 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 3,
+    name: "Premium Package",
+    icon: "/assets/icons/free-plan.svg",
+    price: 199,
+    credits: 2000,
+    inclusions: [
+      {
+        label: "2000 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: true,
+      },
+    ],
+  },
+];
+
+export const transformationTypes = {
+  restore: {
+    type: "restore",
+    title: "Restore Image",
+    subTitle: "Refine images by removing noise and imperfections",
+    config: { restore: true },
+    icon: "image.svg",
+  },
+  removeBackground: {
+    type: "removeBackground",
+    title: "Background Remove",
+    subTitle: "Removes the background of the image using AI",
+    config: { removeBackground: true },
+    icon: "camera.svg",
+  },
+  fill: {
+    type: "fill",
+    title: "Generative Fill",
+    subTitle: "Enhance an image's dimensions using AI outpainting",
+    config: { fillBackground: true },
+    icon: "stars.svg",
+  },
+  remove: {
+    type: "remove",
+    title: "Object Remove",
+    subTitle: "Identify and eliminate objects from images",
+    config: {
+      remove: { prompt: "", removeShadow: true, multiple: true },
     },
-  };
-  
-  export const aspectRatioOptions = {
-    "1:1": {
-      aspectRatio: "1:1",
-      label: "Square (1:1)",
-      width: 1000,
-      height: 1000,
+    icon: "scan.svg",
+  },
+  recolor: {
+    type: "recolor",
+    title: "Object Recolor",
+    subTitle: "Identify and recolor objects from the image",
+    config: {
+      recolor: { prompt: "", to: "", multiple: true },
     },
-    "3:4": {
-      aspectRatio: "3:4",
-      label: "Standard Portrait (3:4)",
-      width: 1000,
-      height: 1334,
-    },
-    "9:16": {
-      aspectRatio: "9:16",
-      label: "Phone Portrait (9:16)",
-      width: 1000,
-      height: 1778,
-    },
-  };
-  
-  export const defaultValues = {
-    title: "",
-    aspectRatio: "",
-    color: "",
-    prompt: "",
-    publicId: "",
-  };
-  
-  export const creditFee = -1;
+    icon: "filter.svg",
+  },
+};
+
+export const aspectRatioOptions = {
+  "1:1": {
+    aspectRatio: "1:1",
+    label: "Square (1:1)",
+    width: 1000,
+    height: 1000,
+  },
+  "3:4": {
+    aspectRatio: "3:4",
+    label: "Standard Portrait (3:4)",
+    width: 1000,
+    height: 1334,
+  },
+  "9:16": {
+    aspectRatio: "9:16",
+    label: "Phone Portrait (9:16)",
+    width: 1000,
+    height: 1778,
+  },
+};
+
+export const defaultValues = {
+  title: "",
+  aspectRatio: "",
+  color: "",
+  prompt: "",
+  publicId: "",
+};
+
+export const creditFee = -1;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,85 +6,91 @@ import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
-// ユーザー作成
+// CREATE
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectToDatabase();  // データベースへ接続
+    await connectToDatabase();
 
-    const newUser = await User.create(user); // 新規ユーザー作成
+    const newUser = await User.create(user);
 
-    return JSON.parse(JSON.stringify(newUser)); // 新規ユーザーのデータを返す
-  } catch (error) {
-    handleError(error); // エラーハンドリング
-  }
-}
-
-// ユーザー取得
-export async function getUserById(userId: string) {
-  try {
-    await connectToDatabase(); // データベースへ接続
-
-    const user = await User.findOne({ clerkId: userId }); // ユーザーIDで検索
-
-    if (!user) throw new Error("User not found"); // ユーザーが見つからない場合のエラー
-
-    return JSON.parse(JSON.stringify(user)); // ユーザーデータを返す
-  } catch (error) {
-    handleError(error); 
-  }
-}
-
-// ユーザー更新
-export async function updateUser(clerkId: string, user: UpdateUserParams) {
-  try {
-    await connectToDatabase(); // データベースへ接続
-
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
-      new: true,
-    }); // ユーザー情報の更新
-
-    if (!updatedUser) throw new Error("User update failed"); // 更新失敗時のエラー
-    
-    return JSON.parse(JSON.stringify(updatedUser)); // 更新後のユーザーデータを返す
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
 }
 
-// ユーザー削除
+// READ
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) throw new Error("User not found");
+
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// UPDATE
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error("User update failed");
+    
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// DELETE
 export async function deleteUser(clerkId: string) {
   try {
-    await connectToDatabase(); // データベースへ接続
+    await connectToDatabase();
 
-    const userToDelete = await User.findOne({ clerkId }); // 削除するユーザーを検索
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
 
-    if (!userToDelete) throw new Error("User not found"); // ユーザーが見つからない場合のエラー
+    if (!userToDelete) {
+      throw new Error("User not found");
+    }
 
-    const deletedUser = await User.findByIdAndDelete(userToDelete._id); // ユーザーの削除
-    revalidatePath("/"); // パスの再検証
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null; // 削除したユーザーのデータを返す
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
   } catch (error) {
-    handleError(error); 
+    handleError(error);
   }
 }
 
-// クレジットの使用
+// USE CREDITS
 export async function updateCredits(userId: string, creditFee: number) {
   try {
-    await connectToDatabase(); // データベースへ接続
-
-    const updatedUserCredits = await User.findOneAndUpdate(
-      { _id: userId },
-      { $inc: { creditBalance: creditFee }},
-      { new: true }
-    ); // クレジットの更新
-
-    if(!updatedUserCredits) throw new Error("User credits update failed"); // 更新失敗時のエラー
-
-    return JSON.parse(JSON.stringify(updatedUserCredits)); // 更新後のクレジット情報を返す
+     console.log(`Updating credits for user ID: ${userId}, credit fee: ${creditFee}`);
+     await connectToDatabase();
+ 
+     const updatedUserCredits = await User.findOneAndUpdate(
+       { _id: userId },
+       { $inc: { creditBalance: creditFee }},
+       { new: true }
+     )
+ 
+     if(!updatedUserCredits) throw new Error("User credits update failed");
+ 
+     console.log(`Updated user credits: ${JSON.stringify(updatedUserCredits)}`);
+     return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
-    handleError(error); 
+     console.error(`Error updating user credits: ${error}`);
+     handleError(error);
   }
 }
-
